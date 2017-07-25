bf00395e71704d6d75ee651af300d052c028f0d3
ahatzikonstantinou <ahatzikonstantinou@gmail.com>
2017-07-25 16:12:39 +0300

    Language selection is stored in localStorage and loaded at startup.

--- web/src/main/webapp/app/components/language/language.controller.js
+++ web/src/main/webapp/app/components/language/language.controller.js
@@ -11,15 +11,34 @@
         var vm = this;
 
         vm.changeLanguage = changeLanguage;
+
         vm.languages = null;
 
         JhiLanguageService.getAll().then(function (languages) {
             vm.languages = languages;
+            // console.log( 'languages: ', languages ); //ahat
         });
 
         function changeLanguage (languageKey) {
+            // console.log( 'changeLanguage to : ', languageKey ); //ahat
             $translate.use(languageKey);
             tmhDynamicLocale.set(languageKey);
+            localStorage.setItem('languageKey', languageKey);   //ahat
         }
+
+        //ahat
+        var languageKey = localStorage.getItem( 'languageKey' );
+        // console.log( 'read languageKey "', languageKey, '" from localStorage' );
+        if( languageKey )
+        {
+            // console.log( 'will use "', languageKey, '" for translation' );
+            $translate.use( languageKey );
+            tmhDynamicLocale.set( languageKey );            
+        }
+        // else
+        // {
+        //     console.log( 'no languageKey was found in localStorage' );
+        // }
+
     }
 })();
